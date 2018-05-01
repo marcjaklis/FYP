92026a683e176945edb565e63c7bd18236bd7a64
Unknown <ryan.chedrawi@gmail.com>
2018-05-01 19:41:50 +0300

    Load image for best of yoga from internet and adding Picasso library

--- app/src/main/java/com/example/fypgymapp/RegularFirstPage.java
+++ app/src/main/java/com/example/fypgymapp/RegularFirstPage.java
@@ -3,11 +3,14 @@ package com.example.fypgymapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.squareup.picasso.Picasso;
 
 public class RegularFirstPage extends AppCompatActivity {
 
     private Button topGyms;
-    private Button featured;
+    private ImageButton yogaBest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,9 @@ public class RegularFirstPage extends AppCompatActivity {
         setContentView(R.layout.featured);
 
         topGyms = (Button) findViewById(R.id.topgyms);
-        featured = (Button) findViewById(R.id.yogabest);
+        yogaBest = (ImageButton) findViewById(R.id.yogabest);
+        yogaBest.setBackground(null);
+        Picasso.with(this).load("https://firebasestorage.googleapis.com/v0/b/fyptest2-38a21.appspot.com/o/bestofyoga.png?alt=media&token=8b19f30e-30ae-46a9-984b-b7b968eece44")
+                .placeholder(R.mipmap.ic_launcher).fit().centerCrop().into(yogaBest);
     }
 }
